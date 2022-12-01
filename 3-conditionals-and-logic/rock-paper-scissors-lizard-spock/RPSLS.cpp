56b7866655c1f0048bded224806fc1275706398b
1NEGROUP <onegroup50@gmail.com>
2022-11-30 18:05:36 -0600

    Update RPSLS.cpp

--- 3-conditionals-and-logic/rock-paper-scissors-lizard-spock/RPSLS.cpp
+++ 3-conditionals-and-logic/rock-paper-scissors-lizard-spock/RPSLS.cpp
@@ -1,99 +1,47 @@
-/*
-This program will allow you to play RPSLS against the computer.
-*/
+/* Scissor Cuts Paper
+  Paper Wraps Rock
+  Rock Crushes Lizard
+  Lizard Poisens Spock
+  Spock Vaporizes Scissors
+  Scissors Beheads Lizard
+  Lizard eats Paper
+  Paper Disproves Spock.*/
 
-#include <iostream>
-#include <cstdlib>
+  #include <iostream>
+  #include <stdlib.h>
 
-int main() {
+  int main(){
+    srand (time(NULL));
+    int Rock, Paper, Scissors, Lizard, Spock;
+    int Choice;
+    int CompChoice = rand() % 5 +1;
 
-  srand(time(NULL));
-  int computer = std::rand() % 5 + 1;
-  int user = 0;
-  
-  std::cout << "=================================\n";
-  std::cout << "rock paper scissors lizard spock!\n";
-  std::cout << "=================================\n";
+    std::cout << "Be ready to Pick\n" <<"Rock = 1, Paper = 2, Scissors = 3, Lizard = 4, Spock = 5\n";
 
-  std::cout << "1) ✊\n";
-  std::cout << "2) ✋\n";
-  std::cout << "3) ✌️\n";
-  std::cout << "4) lizard\n";
-  std::cout << "5) spock\n\n";
+    std::cin >> Choice;
 
-  std::cout << "shoot: ";
-  
-  int rock = 1;
-  int paper = 2;
-  int scissors = 3;
-  int lizard = 4;
-  int spock = 5;
-  
-  std::cin >> user;
-  
-  /* 
-  rock > scissors
-  scissors > paper
-  paper > rock
-  lizard > spock
-  spock > scissors
-  scissors > lizard
-  lizard > paper
-  paper > spock
-  spock > rock
-  rock > lizard
-  */
-  
-  std::cout << "computer: " << computer << "\n\n";
-  
-  if (user == rock && computer == scissors) {
-      
-    std::cout << "You Win!\n";
-    
-  } else if (user == scissors && computer == paper) {
-    
-    std::cout << "You Win!\n";
-    
-  } else if (user == paper && computer == rock) {
-    
-    std::cout << "You Win!\n";
-      
-  } else if (user == lizard && computer == spock) {
-      
-    std::cout << "You Win!\n";
-      
-  } else if (user == spock && computer == scissors) {
-      
-    std::cout << "You Win!\n";
-      
-  } else if (user == scissors && computer == lizard) {
-      
-    std::cout << "You Win!\n";
-      
-  } else if (user == lizard && computer == paper) {
-      
-    std::cout << "You Win!\n";
-      
-  } else if (user == paper && computer == spock) {
-      
-    std::cout << "You Win!\n";
-      
-  } else if (user == spock && computer == rock) {
-      
-    std::cout << "You Win!\n";
-      
-  } else if (user == rock && computer == lizard) {
-      
-    std::cout << "You Win!\n";
-      
-  } else if (user == computer) {
-      
-    std::cout << "Tie!\n";
-      
-  } else {
-      
-    std::cout << "You Lose!\n";
-      
+    if (Choice == 1 || CompChoice == 1){
+      Rock = 1;
+    } else if (Choice == 2 || CompChoice == 2){
+      Paper = 1;
+    } else if (Choice == 3 || CompChoice == 3){
+      Scissors = 1;
+    } else if (Choice == 4 || CompChoice == 4){
+      Lizard = 1;
+    } else if (Choice == 5 || CompChoice == 5){
+      Spock = 1;
+    }
+
+    if ( Scissors == 1 && Paper == 1 || Lizard == 1){
+      std::cout << "Scissors Wins.\n";
+    } else if (Paper == 1 && Rock == 1 || Spock == 1){
+      std::cout << "Paper Wins.\n";
+    } else if (Rock == 1 && Lizard == 1 || Scissors == 1){
+      std::cout << "Rock Wins.\n";
+    } else if (Lizard == 1 && Spock == 1 || Paper == 1){
+      std::cout << "Lizard Wins.\n";
+    } else if (Spock == 1 && Rock == 1 || Scissors == 1){
+      std::cout << "Spock Wins.\n";
+    }
+
   }
-  
-}
